bf8386678f9a37e4aaccc271ad756ccb8feda074
Alex <alexmankiev@gmail.com>
2023-02-03 20:22:19 +0200

    Калькулятор с применением for и if.

--- service/src/main/java/ua/edson/calculator/Application.java
+++ service/src/main/java/ua/edson/calculator/Application.java
@@ -1,45 +1,49 @@
 package ua.edson.calculator;
 
+// Написать цикл for, идеально сделать его бесконечным.
+// В теле цикла вводить 2 числа и математический цикл. Высчитывать выражение.
+
 import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        System.out.println("Введите два числа и одну из арифметических операций \"+\" \"-\" \"*\" \"/\" ");
+        for (; ;) {
+            System.out.println("Введите два числа и одну из арифметических операций \"+\" \"-\" \"*\" \"/\" ");
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        Integer a = in.nextInt();
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите первое число: ");
+            Integer a = in.nextInt();
 
-        System.out.println("Введите математический знак: ");
-        String s = in.next();
+            System.out.println("Введите математический знак: ");
+            String s = in.next();
 
-        System.out.println("Введите второе число: ");
-        Integer b = in.nextInt();
-        //System.out.print(a + s + b + "=");
+            System.out.println("Введите второе число: ");
+            Integer b = in.nextInt();
+            //System.out.print(a + s + b + "=");
 
-        //TODO: тут писать код
+            //TODO: тут писать код
 
-        int add, subtract, multiply;
-        double devide;
+            int add, subtract, multiply;
+            double devide;
 
-        add = a + b;
-        subtract = a - b;
-        multiply = a * b;
-        devide = (double) a / b;
+            add = a + b;
+            subtract = a - b;
+            multiply = a * b;
+            devide = (double) a / b;
 
-        if (s.equals("+")) {
-            System.out.println("Сумма чисел равна = " + add);
-        } else if (s.equals("-")) {
-            System.out.println("Разница чисел равна = " + subtract);
-        } else if (s.equals("*")) {
-            System.out.println("Умножение чисел равно = " + multiply);
-        } else if (s.equals("/")) {
-            System.out.println("Деление чисел равно = " + devide);
-        } else {
-            System.out.println("Задана не верная арифметическая операция!!!");
+            if (s.equals("+")) {
+                System.out.println("Сумма чисел равна = " + add);
+            } else if (s.equals("-")) {
+                System.out.println("Разница чисел равна = " + subtract);
+            } else if (s.equals("*")) {
+                System.out.println("Умножение чисел равно = " + multiply);
+            } else if (s.equals("/")) {
+                System.out.println("Деление чисел равно = " + devide);
+            } else {
+                System.out.println("Задана не верная арифметическая операция!!!");
+            }
         }
-
     }
 }
